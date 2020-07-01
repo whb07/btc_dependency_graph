3b3415481bd857a9a5e614a3f319a0885f607163
William Bright <williambright91@gmail.com>
2020-06-30 21:34:25 -0400

    initial

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.IO
+open Microsoft.FSharp.Core
+// File.ReadAllLines
+
+[<EntryPoint>]
+let main argv =
+    let x = 10
+    printfn "Hello World from F#! %d" x
+    0 // return an integer exit code
