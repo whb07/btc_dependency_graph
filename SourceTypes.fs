ec5849cd6ca64cb0c7547eada567bd7e30186b76
William Bright <williambright91@gmail.com>
2020-07-02 12:42:08 -0400

    removed excess spacing

--- SourceTypes.fs
+++ SourceTypes.fs
@@ -1,17 +1,9 @@
 module SourceTypes
 
-
 type SourceCode = 
-
     | HeaderFile of string
-
     | SourceFile of string
 
-
-
-
 type Library = 
-
     | External of string
-
     | Internal of string
